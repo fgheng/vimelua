eb488d6f6ab67da77624e67a34ebd4140ed570b3
forever <redusty@163.com>
2022-06-15 23:59:56 +0800

    init

--- lua/plugins/completion/copilot.lua
+++ lua/plugins/completion/copilot.lua
@@ -1,23 +1,22 @@
--- local map = require('utils').map
--- local opts = { silent = true, noremap = true }
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {
+    ["*"] = false,
+    ["javascript"] = true,
+    ["typescript"] = true,
+    ["lua"] = true,
+    ["rust"] = true,
+    ["c"] = true,
+    ["c#"] = true,
+    ["c++"] = true,
+    ["go"] = true,
+    ["python"] = true,
+}
 
--- vim.g.copilot_no_tab_map = true
--- map('i', '<c-l>', '<c-u>call copilot#Accept("")<cr>', opts)
 
 vim.cmd [[
-    let g:copilot_filtype = {
-        \ 'TelescopePrompt': v:false,
-        \ 'TelescopeResults': v:false,
-        \ 'dap-ui-debug-prompt': v:false,
-        \ 'dap-prompt': v:false,
-        \ 'prompt': v:false,
-    \ }
-
     let g:copilot_no_tab_map = v:true
     imap <silent><script><expr> <m-l> copilot#Accept("")
     imap <silent> <m-h> <Plug>(copilot-dismiss)
     imap <silent> <m-L> <Plug>(copilot-next)
     imap <silent> <m-H> <Plug>(copilot-previous)
 ]]
--- local opts = {silent = true}
--- vim.keymap.set('i', '<M-l>', '<c-u><script><expr>call copilot#Accept("")<cr>', opts)
